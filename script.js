2bf3f1f3c8e68a8f4c833c5ec75e2bec28d37768
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-11-20 12:50:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+function mapLetters(str) {
+  let letterMap = new Map();
+
+  for (let i = 0; i < str.length; i++) {
+    letterMap.set(str.charAt(i), []);
+  }
+  // console.log(letterMap);
+
+  letterMap.forEach((val, key) => {
+    let charindex = [];
+    for (let i = 0; i < str.length; i++) {
+      if (key == str[i]) {
+        charindex.push(i);
+      }
+    }
+    letterMap.set(key, charindex);
+  });
+
+  // console.log(letterMap);
+  // Convert Map to Object
+  const myObject = {};
+  letterMap.forEach((value, key) => {
+    myObject[key] = value;
+  });
+
+  return myObject;
+}
